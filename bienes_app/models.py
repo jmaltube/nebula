78c0762dd4c5292e84fa409178b02b6ee56daef4
nebula <juan.altube@nebula.com.ar>
2016-12-12 12:10:24 -0300

    MIgracion 01__v6

--- bienes_app/models.py
+++ bienes_app/models.py
@@ -287,7 +287,8 @@ class Bien(models.Model):
     ubicacion = models.CharField(max_length=100, blank=True, null=True)
     peso = models.DecimalField (max_digits=10, decimal_places=2,blank=True, null=True)
     volumen = models.DecimalField (max_digits=10, decimal_places=2, blank=True, null=True)
-
+    metroxkg = models.DecimalField (max_digits=10, decimal_places=2, blank=True, null=True)
+    
     def __str__(self):
         return "{0} ({1})".format(self.denominacion, self.codigo)
 
